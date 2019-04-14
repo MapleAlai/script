36223220f2eb98ad877a0e103fd10492b03cac52
MapleAlai <amqqfkhk@qq.com>
2019-04-14 23:00:31 +0800

    add esp

--- script/git/init.sh
+++ script/git/init.sh
@@ -10,11 +10,11 @@ fi
 read -p "请输入用户名：" user_name
 read -p "请输入邮箱：" user_email
 
-if ! ifon "是否使用用户名：$user_name ?";then
+if ! ifon "是否使用用户名:$user_name?";then
     read -p "请输入用户名：" user_name
 fi
 
-if ! ifon "是否使用邮箱：$user_email ?";then
+if ! ifon "是否使用邮箱:$user_email?";then
     read -p "请输入邮箱：" user_email
 fi
 
