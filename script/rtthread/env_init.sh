2dd87b2003f5bb244070814960d5c64317906e9b
MapleRian <amqqfkhk@qq.com>
2020-06-29 17:04:47 +0800

    添加 Linux 下 RT-Thread 环境的初始化脚本

--- script/rtthread/env_init.sh
+++ script/rtthread/env_init.sh
@@ -44,7 +44,7 @@ fi
 cpu_processor=`expr $(grep -c 'processor' /proc/cpuinfo) \* 2`
 #  编译
 result=$(scons -j$cpu_processor)
-success=$(echo $success | grep -A 3 -i "hex filename")
+success=$(echo $result | grep -A 3 -i "filename")
 if [ $success ];then
   echo "编译成功"
   echo $success
